be0faad8a3c625dce0783e5aad3b2cd65da4de5f
Henrique Peron <henrique.peron9@gmail.com>
2022-12-28 16:29:17 -0300

    delay on random bot

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 import GameRunner.GameRunner;
 import GameUI.ProcessingRenderer.ProcessingRenderer;
 // import GameUI.TerminalRenderer.TerminalUIRenderer;
-// import Bots.RandomBot;
-// import GameRunner.BotGame;
+import Bots.RandomBot;
+import GameRunner.BotGame;
 
 public class App {
     final static private int BOARD_WIDTH = 10;
@@ -10,16 +10,35 @@ public class App {
     final static private int N_PIECES = 3;
 
     public static void main(String[] args) {
-        // Tests.runTests();
+        // GameRunner gameRunner = new GameRunner(
+        // BOARD_WIDTH,
+        // BOARD_HEIGHT,
+        // "Player 1",
+        // new TerminalUIRenderer(),
+        // N_PIECES);
 
-        // GameRunner gameRunner = new GameRunner(BOARD_WIDTH, BOARD_HEIGHT,
-        // "Player 1", new TerminalUIRenderer(), 3);
+        // GameRunner gameRunner = new BotGame(
+        // BOARD_WIDTH,
+        // BOARD_HEIGHT,
+        // "Random Bot",
+        // new TerminalUIRenderer(),
+        // N_PIECES,
+        // new RandomBot());
 
-        // GameRunner gameRunner = new BotGame(BOARD_WIDTH, BOARD_HEIGHT,
-        // "Bot", new TerminalUIRenderer(), 3, new RandomBot());
+        // GameRunner gameRunner = new GameRunner(
+        // BOARD_WIDTH,
+        // BOARD_HEIGHT,
+        // "Player 1",
+        // new ProcessingRenderer(BOARD_WIDTH, BOARD_HEIGHT, N_PIECES),
+        // N_PIECES);
 
-        GameRunner gameRunner = new GameRunner(BOARD_WIDTH, BOARD_HEIGHT,
-                "Player 1", new ProcessingRenderer(BOARD_WIDTH, BOARD_HEIGHT, N_PIECES), N_PIECES);
+        GameRunner gameRunner = new BotGame(
+                BOARD_WIDTH,
+                BOARD_HEIGHT,
+                "Random Bot",
+                new ProcessingRenderer(BOARD_WIDTH, BOARD_HEIGHT, N_PIECES),
+                N_PIECES,
+                new RandomBot());
 
         gameRunner.startGame();
     }
--- src/Bots/RandomBot.java
+++ src/Bots/RandomBot.java
@@ -1,7 +1,9 @@
 package Bots;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
+import java.util.Map.Entry;
 
 import GameLogic.GameBoard;
 import GameLogic.Move;
@@ -11,8 +13,23 @@ public class RandomBot implements Bot {
 
     @Override
     public Move getMove(GameBoard gameBoard, Map<Integer, Piece> pieces) {
+        ArrayList<Move> moves = new ArrayList<>();
 
-        Random dice = new Random();
-        return new Move(dice.nextInt(3), dice.nextInt(gameBoard.width), dice.nextInt(gameBoard.height));
+        for (Entry<Integer, Piece> entry : pieces.entrySet())
+            for (int i = 0; i < gameBoard.width; i++)
+                for (int j = 0; j < gameBoard.height; j++)
+                    if (gameBoard.isMoveValid(entry.getValue(), i, j))
+                        moves.add(new Move(entry.getKey(), i, j));
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return moves.get(new Random().nextInt(moves.size()));
+
+        // Random dice = new Random();
+        // return new Move(dice.nextInt(3), dice.nextInt(gameBoard.width),
+        // dice.nextInt(gameBoard.height));
     }
 }
